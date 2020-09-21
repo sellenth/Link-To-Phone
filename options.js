1b17fb5c63738aa939f7232618d6f7eec4caf2c6
sellenth <sellenth@oregonstate.edu>
2020-09-20 21:49:38 -0700

    emailJS escapes URL characters, need to make custom backend

--- options.js
+++ options.js
@@ -1,15 +1,37 @@
-let page = document.getElementById("buttonDiv");
-const kButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement("button");
-    button.style.backgroundColor = item;
-    button.addEventListener("click", function () {
-      chrome.storage.sync.set({ color: item }, function () {
-        console.log("color is " + item);
-      });
-    });
-    page.appendChild(button);
-  }
+function save_options(e) {
+  e.preventDefault();
+  const number = document.getElementById("number").value;
+  const carrier = document.getElementById("carrier").value;
+  chrome.storage.sync.set(
+    {
+      number: number,
+      carrier: carrier,
+    },
+    function () {
+      // Update status to let user know options were saved.
+      var status = document.getElementById("status");
+      status.textContent = "Options saved.";
+      setTimeout(function () {
+        status.textContent = "";
+      }, 1500);
+    }
+  );
 }
-constructOptions(kButtonColors);
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get(
+    {
+      number: "0123456789",
+      carrier: "att",
+    },
+    function (items) {
+      document.getElementById("number").value = items.number;
+      document.getElementById("carrier").value = items.carrier;
+    }
+  );
+}
+document.addEventListener("DOMContentLoaded", restore_options);
+document.getElementById("submit").addEventListener("click", save_options);
