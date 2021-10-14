5cbd713dcbc21f784d7919286e2fe5d4955378bc
Halston <sellenth@oregonstate.edu>
2021-10-14 15:08:48 -0700

    don't allow empty username or password

--- popup.js
+++ popup.js
@@ -2,9 +2,9 @@ let sendSMS = document.getElementById("sendSMS");
 
 sendSMS.onclick = function () {
   chrome.storage.sync.get(["phoneNumber", "password"], function ({phoneNumber, password}) {
-    if (phoneNumber === undefined || password === undefined) {
+    if (!phoneNumber || !password) {
       alert(
-        'Please set your user phone number and password by right clicking this extension\'s icon and clicking on "Options"'
+        'Please set your user phone number and password by right clicking this extension\'s ICON (the one in the toolbar, not the big green button) and clicking on "Options"'
       );
     } else {
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -13,7 +13,6 @@ sendSMS.onclick = function () {
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ phoneNumber: `+1${phoneNumber}`, password: password, msgContent: tabs[0].url })
         };
-        console.log(requestOptions)
         fetch('https://www.link-to-phone.com/outgoingSMS', requestOptions).then(response => {console.log(response)})
       });
     }
